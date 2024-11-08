8bb291e49ad4c86531dcbbe84d6bea341832aa40
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2024-11-08 18:41:28 -0500

    Added a view for the management section

---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,8 @@
+
+<x-app-layout>
+    <p>This is the main content of the management page.</p>
+    <p>More details about managing resources.</p>
+    <div class="container">
+
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/management', function() {
+     return view('management.index');
+    });
+
 require __DIR__.'/auth.php';
